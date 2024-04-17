0d0b24a92e24c6db5a54881a7ebaf14742f63dda
Corentin <ccouasno@gmail.com>
2024-04-17 14:34:11 +0200

    Folder structure + little camera effect

---
+++ Assets/Scripts/CameraFollowMouse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollowMouse : MonoBehaviour
+{
+    [SerializeField] Transform _transform;
+    [SerializeField] Camera _camera;
+    [SerializeField] float _xRange;
+    [SerializeField] float _yRange;
+    [SerializeField] float _xOffset;
+    [SerializeField] float _yOffset;
+
+    void Update()
+    {
+        var pointerPosition = _camera.ScreenToViewportPoint(Input.mousePosition);
+        
+        _transform.position = new Vector3(
+            Mathf.Lerp(-_xRange, _xRange, pointerPosition.x) + _xOffset,
+            Mathf.Lerp(-_yRange, _yRange, pointerPosition.y) + _yOffset,
+            -10
+        );
+    }
+}
